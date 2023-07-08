dbaa96633348b4288f1d35ff52e00a42e88c29a4
Cozyamy <cozyamy@gmail.com>
2023-07-08 08:49:34 +0100

    index page without reponsiveness is done

--- js/script.js
+++ js/script.js
@@ -3,3 +3,6 @@ const modeToggleBtn = document.getElementById('mode-toggle');
 modeToggleBtn.addEventListener('click', function() {
   document.body.classList.toggle('dark-mode');
 });
+/* End of change mode */
+/* Start of index carousel */
+
