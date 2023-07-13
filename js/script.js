dc8696b1efc03d2c2fddeb28af14228c276cfba9
Cozyamy <cozyamy@gmail.com>
2023-07-13 12:30:23 +0100

    started reponsiveness for index page

--- js/script.js
+++ js/script.js
@@ -4,5 +4,14 @@ modeToggleBtn.addEventListener('click', function() {
   document.body.classList.toggle('dark-mode');
 });
 /* End of change mode */
-/* Start of index carousel */
+/* Start of hamburger menu */
 
+document.addEventListener('DOMContentLoaded', function() {
+  var hamburgerMenu = document.querySelector('.hamburger');
+  var navLinks = document.querySelector('.nav-links');
+
+  hamburgerMenu.addEventListener('click', function() {
+    this.classList.toggle('active');
+    navLinks.classList.toggle('active');
+  });
+});
